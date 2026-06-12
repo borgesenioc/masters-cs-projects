ad9b68d09fa3d3efd5804214ad6817f8eac7d807
borgesenioc <borgesenioc@gmail.com>
2026-06-12 10:03:42 -0300

    assignment: implementing the greedy algorithim pseudocode for max cut

--- linear-programming-approx-algorithms/module_3/programming_assignment_3/problem_1.py
+++ linear-programming-approx-algorithms/module_3/programming_assignment_3/problem_1.py
@@ -1,5 +1,6 @@
 import networkx as nx
 from matplotlib import pyplot as plt 
+from random import *
 
 '''
 
@@ -107,4 +108,87 @@ Write down the pseudocode and prove that the algorithm terminates/find its time 
 
 Note These problems are meant for you to develop your own thinking/problem solving and will not be graded due to the online nature of this class. We will be grading just the programming assignments based on test cases. The answers to select questions are given at the end of the notebook.
 '''
+
+# My answer
+'''
+MaxCutGreedy(G = (V, E)):
+
+# divide the initial partition arbitrarily
+S1 are the initial n/2 of V
+S2 are the remaining n/2 of V
+
+# evaluate if each node of each set dividion is imbalanced
+while there exists an imbalanced vertex v in V:
+  flip v to the opposite partition
+
+return (S1, S2)
+'''
+
+'''
+P2: Implement the Greedy Algorithm
+Now let's implement the greedy algorithm to find a cut of the graph with no imbalanced vertices.
+
+The graph will be given as an adjacency list representation.
+
+Vertex set will be  {0,…,𝑛−1}
+  where  𝑛
+  (the number of vertices) is a input parameter.
+adj_list is an adjacency list which is given as a list of sts. For instance adj_list[i] for a vertex  𝑖
+  is a set to all vertices connected to vertex  𝑖
+ .
+Note that since the graph is undirected, if  𝑗
+  lies in adj_list[i], we know that  𝑖
+  will be in adjacency_list[j].
+You can assume that the graph has no self loops or multiple edges between same pairs of nodes.
+We will specify a cut as a list of  𝑛
+  boolean values [b0, b1,...bn-1] wherein bi is True if  𝑖∈𝑆1
+ , and False if vertex  𝑖∈𝑆2
+ .
+Implement the overall function find_balanced_cut that takes a graph as input and returns a list of Booleans specifying the final cut obtained by running the greedy algorithm. Please pay attention to efficiency, we will be running some large graphs through your code and it should run within a few seconds on graphs with thousands of nodes.
+'''
+
+# This is the function that you will implement the greedy algorithm
+# It should input a graph as an adjacency list and return a partition where 
+# every vertex is balanced, as described above.
+# Return a list of Booleans of size n, wherein for any vertex v, if list[v] = True then 
+# it belongs to partition S1 otherwise to partition S2.
+def find_balanced_cut(n, adj_list): 
+    assert n >= 1
+    assert len(adj_list) == n
+    # Check that the adjacency list makes sense and represents a directed graph
+    for (i, neighbors) in enumerate(adj_list):
+        assert all( 0 <= j < n for j in neighbors )
+        assert i not in neighbors # no self loops allowed
+        for j in neighbors: 
+            assert i in adj_list[j]
+    # just start with an initial cut tthat places first n/2 nodes in S1 and rest in S2.
+    cut = [True if i < n/2 else False for i in range(n)]
+    ## TODO: now run the greedy algorithm. It will be helpful to have helper functions to find 
+    ## imbalanced_vertices, maintain an array with the number of edges for each node that are cut and so on.
+    ## Note: your algorithm must return a cut where all nodes are balanced.
+    # your code here
+    raise NotImplementedError
+
+#These  are useful functions for the test cases
+# IMPORTANT: 
+# Please ensure that you run these cells before running test cases or else you may get unknown function errors.
+
+# Make an adjacency list out of a list of edges.
+def mk_adjacency_list(n, edge_list):
+    adj_list = [set() for i in range(n)]
+    for (i,j) in edge_list:
+        adj_list[i].add(j)
+        adj_list[j].add(i)
+    return adj_list
+
+# Test Partition
+def test_cut(n, adj_list, cut):
+    num_edges_crossing_cut = [0]*n
+    for (i, neighbors) in enumerate(adj_list):
+        num_edges_crossing_cut[i] = sum([cut[i] != cut[j] for j in neighbors])
+        if 2 * num_edges_crossing_cut[i] < len(neighbors):
+            assert False, f'Test Failed: In your cut, vertex {i} has {len(neighbors)} edges incident on it but only {num_edges_crossing_cut[i]} edges cross the cut'
+    return 
+
+
 
